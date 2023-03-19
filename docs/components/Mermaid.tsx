74ddbb7761ddee985070b4ac5cbb0ffd23597967
pengzhanbo <volodymyr@foxmail.com>
2023-03-19 13:09:26 +0800

    update post

--- docs/components/Mermaid.tsx
+++ docs/components/Mermaid.tsx
@@ -4,24 +4,29 @@ import { useTheme } from 'nextra-theme-docs'
 
 let uuid = 0
 
-export default function Mermaid({ chart }) {
+export default function Mermaid({ chart, theme = 'default' }: MermaidProps) {
   const [svg, setSVG] = useState('')
-  const { theme } = useTheme()
+  const { theme: themeMode } = useTheme()
 
   useEffect(() => {
     mermaid.initialize({
       startOnLoad: true,
-      theme: theme === 'dark' ? 'dark' : 'forest'
+      theme: themeMode === 'dark' ? 'dark' : theme
     })
     mermaid.render(
       `mermaid-svg-${uuid}`,
       chart,
     ).then(({svg}) => setSVG(svg))
     uuid++
-  }, [chart, theme])
+  }, [chart, theme, themeMode])
 
   return <div
     className='flex justify-center pt-5'
     dangerouslySetInnerHTML={{ __html: svg }}
   />
+}
+
+export type MermaidProps = {
+  chart: string
+  theme?: 'default' | 'neutral' | 'dark' | 'forest' | 'base'
 }
